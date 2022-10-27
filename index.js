0aef3322bda2b02fd835b4cf6ad5cfb39e186634
陳俊廷 <jyun_chen@addcn.com>
2022-10-27 15:13:34 +0800

    20221027-1513

--- index.js
+++ index.js
@@ -1,37 +1,33 @@
-const { strictEqual } = require("assert");
 const express = require("express");
 const app = express();
-
-const mysql = require("mysql");
-
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+// const userRoute = require("./routes/user");
+// const authRoute = require("./routes/auth");
+// const productRoute = require("./routes/product");
+// const cartRoute = require("./routes/cart");
+// const orderRoute = require("./routes/order");
+// const stripeRoute = require("./routes/stripe");
+// const cors = require("cors");
 
 dotenv.config();
 
-var pool = mysql.createPool({
-  // user: process.env.MYSQL_ACCOUNT,
-  // password: process.env.MYSQL_PASSWORD,
-  user: 'root',
-  password: '',
-  host: 'localhost',
-  port: '3306',
-  database: 'Shoppingcart',
-  waitForConnections : true,
-  connectionLimit : 10 //連線數上限
-})
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.get("/api/test", () => {
-  console.log("Api is sucessful")
-})
+// app.use(cors());
+// app.use(express.json());
+// app.use("/api/auth", authRoute);
+// app.use("/api/users", userRoute);
+// app.use("/api/products", productRoute);
+// app.use("/api/carts", cartRoute);
+// app.use("/api/orders", orderRoute);
+// app.use("/api/checkout", stripeRoute);
 
-pool.getConnection((err,connection) => {
-  if(err){
-    console.log(err)
-  }else{
-    console.log("Connection Sucessfully!")
-  }
-})
-
-app.listen(process.env.PORT || 5000,() => {
-  console.log("running!")
-})
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend server is running!");
+});
